327484e80808e943562675e92f45fac7aa5fee0d
pradeepy316694 <softkiller706@gmail.com>
2021-03-11 23:25:33 +0530

    Addition Of A and B

---
+++ AddNum.java
@@ -0,0 +1,10 @@
+public class AddNum {
+     public static void main(String[] args) {
+         int a = 30;
+         int b= 20;
+         int c= a+b;
+         System.out.println("Addition of A and B is:"+c);
+        
+    }
+    
+}
